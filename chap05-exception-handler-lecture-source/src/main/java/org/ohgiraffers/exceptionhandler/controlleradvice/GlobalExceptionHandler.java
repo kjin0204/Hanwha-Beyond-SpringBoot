bf4f8bb65bcf56d7f7281fac898f3220a145cef2
HyeonggyuKang <kin0204@naver.com>
2025-09-10 13:58:53 +0900

    예외 정의 하지 않은 모든 예외처리 예외 페이지 forwarding 처리

--- chap05-exception-handler-lecture-source/src/main/java/org/ohgiraffers/exceptionhandler/controlleradvice/GlobalExceptionHandler.java
+++ chap05-exception-handler-lecture-source/src/main/java/org/ohgiraffers/exceptionhandler/controlleradvice/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package org.ohgiraffers.exceptionhandler.controlleradvice;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -23,5 +25,16 @@ public class GlobalExceptionHandler {
     public String handleIllegalArgumentException(GlobalException e, Model model){
         model.addAttribute("exceptionMessage","전역에서 처리된 인수 예외: " + e.getMessage());
         return "error/default";
+    }
+
+    // ★ 정의되지 않은 모든 예외 (catch-all)
+    @ExceptionHandler(Exception.class)
+    public String handleOtherExceptions(Exception e, HttpServletRequest req, Model model){
+        System.out.println("Unhandled exception at " + req.getMethod() +" , " + req.getRequestURI());
+        model.addAttribute("exceptionMessage", "알 수 없는 예외가 발생했습니다.");
+        // (선택) 상태코드도 500으로 명시하고 싶다면:
+        req.setAttribute("javax.servlet.error.status_code", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        return "error/default"; // 한 페이지로 포워드 (뷰 이름)
+        // 혹은 "forward:/error/unexpected"로 강제 forward 가능
     }
 }
